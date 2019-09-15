8cc7d196ba6e3ef37013b6f6b4758e7fcca557ec
Dat Chung <datchung@users.noreply.github.com>
2019-09-15 00:05:03 -0400

    Define components structure

--- app.js
+++ app.js
@@ -5,11 +5,70 @@ var Route = ReactRouterDOM.Route;
 
 function App() {
   return React.createElement(
-    "h1",
+    React.Fragment,
     null,
-    "Todo App"
+    React.createElement(
+      "h1",
+      null,
+      "Todo App"
+    ),
+    React.createElement(TodoListPage, null)
   );
 }
+
+// Todo list page
+function TodoListPage() {
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      "p",
+      null,
+      "TodoListPage"
+    ),
+    React.createElement(TodoList, null)
+  );
+}
+
+// Todo list smart
+function TodoList() {
+  return React.createElement(TodoListSimple, null);
+}
+
+// Todo list simple
+function TodoListSimple() {
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      "p",
+      null,
+      "TodoListSimple"
+    ),
+    React.createElement(Todo, null)
+  );
+}
+
+// Todo smart
+function Todo() {
+  return React.createElement(TodoSimple, null);
+}
+
+// Todo simple
+function TodoSimple() {
+  return React.createElement(
+    "p",
+    null,
+    "TodoSimple"
+  );
+}
+
+// Add/Edit/Delete Todo page
+
+// Add/Edit/Delete Todo smart
+
+// Add/Edit/Delete Todo simple
+
 
 ReactDOM.render(React.createElement(
   Router,
--- src/app.js
+++ src/app.js
@@ -5,9 +5,60 @@ const Route = ReactRouterDOM.Route;
 
 function App() {
   return (
-    <h1>Todo App</h1>
+    <React.Fragment>
+      <h1>Todo App</h1>
+      <TodoListPage></TodoListPage>
+    </React.Fragment>
   );
 }
+
+// Todo list page
+function TodoListPage() {
+  return (
+    <React.Fragment>
+      <p>TodoListPage</p>
+      <TodoList></TodoList>
+    </React.Fragment>
+  );
+}
+
+// Todo list smart
+function TodoList() {
+  return (
+    <TodoListSimple></TodoListSimple>
+  );
+}
+
+// Todo list simple
+function TodoListSimple() {
+  return (
+    <React.Fragment>
+      <p>TodoListSimple</p>
+      <Todo></Todo>
+    </React.Fragment>
+  );
+}
+
+// Todo smart
+function Todo() {
+  return (
+    <TodoSimple></TodoSimple>
+  );
+}
+
+// Todo simple
+function TodoSimple() {
+  return (
+    <p>TodoSimple</p>
+  );
+}
+
+// Add/Edit/Delete Todo page
+
+// Add/Edit/Delete Todo smart
+
+// Add/Edit/Delete Todo simple
+
 
 ReactDOM.render(
   <Router>
