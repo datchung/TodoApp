83ded5569b8a38df27c83a1a5586d529ef2379e3
Dat Chung <datchung@users.noreply.github.com>
2019-09-28 14:02:41 -0400

    Change ManageTodoPage subtitle to add or edit

--- bundle.js
+++ bundle.js
@@ -41418,6 +41418,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -41431,6 +41433,19 @@ var _ManageTodo2 = _interopRequireDefault(_ManageTodo);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function ManageTodoPage(props) {
+  var _useState = (0, _react.useState)(""),
+      _useState2 = _slicedToArray(_useState, 2),
+      subTitle = _useState2[0],
+      setSubTitle = _useState2[1];
+
+  (0, _react.useEffect)(function () {
+    var id = props.match.params.id;
+    var todoById = props.todos.find(function (t) {
+      return t.id === id;
+    });
+    setSubTitle(todoById ? "Edit Todo" : "Add Todo");
+  }, [props.match.params.id]);
+
   return _react2.default.createElement(
     _react2.default.Fragment,
     null,
@@ -41438,9 +41453,9 @@ function ManageTodoPage(props) {
       'div',
       { className: 'content' },
       _react2.default.createElement(
-        'p',
+        'h2',
         null,
-        'Todo Form'
+        subTitle
       )
     ),
     _react2.default.createElement(
--- src/components/pages/ManageTodoPage.js
+++ src/components/pages/ManageTodoPage.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, withRouter} from 'react-router-dom'
 import ManageTodo from '../smart/ManageTodo';
 
 function ManageTodoPage(props) {
+  const [subTitle, setSubTitle] = useState("");
+
+  useEffect(() => {
+    const id = props.match.params.id;
+    var todoById = props.todos.find(t => t.id === id);
+    setSubTitle(todoById ? "Edit Todo" : "Add Todo");
+  }, [props.match.params.id]);
+
   return (
     <React.Fragment>
       <div className="content">
-        <p>Todo Form</p>
+        <h2>{subTitle}</h2>
       </div>
       <div className="content">
         <Link to="/">
