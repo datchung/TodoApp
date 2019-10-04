28d45f7340a82bbabcc329cc95e29893416cd3ac
Dat Chung <datchung@users.noreply.github.com>
2019-10-03 21:02:21 -0400

    Remove commented code

--- src/components/simple/FilterSortSimple.js
+++ src/components/simple/FilterSortSimple.js
@@ -6,37 +6,6 @@ function FilterSortSimple(props) {
       <div className="columns is-mobile">
         <div className="column"></div>
         <div className="column is-narrow">
-          {/* <div className="field">
-            <label className="label">Show</label>
-            <div className="control">
-              <div className="select">
-                <select
-                  value={props.selectedFilter}
-                  onChange={props.onFilter}
-                  >
-                  <option value="all">All</option>
-                  <option value="notDone">Not Done</option>
-                  <option value="done">Done</option>
-                </select>
-              </div>
-            </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Sort</label>
-            <div className="control">
-              <div className="select">
-                <select
-                  value={props.selectedSort}
-                  onChange={props.onSort}
-                  >
-                  <option value="newestFirst">Date new to old</option>
-                  <option value="oldestFirst">Date old to new</option>
-                </select>
-              </div>
-            </div>
-          </div> */}
-
           <div className="field is-horizontal">
             <div className="field-label is-normal">
               <label className="label">Show</label>
